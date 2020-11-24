2d294b3b0d45d36f8fe01321d32a75d2446d41b8
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-11-24 14:13:40 +0530

    added actual recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,33 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from './recipe.model';
-
+import { Ingredient } from "../shared/ingredient.model";
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test','test description','https://i0.wp.com/www.growblooms.com/wp-content/uploads/2018/01/recipe.png?w=357&ssl=1'),
-    new Recipe('A test 2','test description','https://i0.wp.com/www.growblooms.com/wp-content/uploads/2018/01/recipe.png?w=357&ssl=1')
+    new Recipe(
+      'Cheese Burger',
+      'Cheese Burger',
+      'https://pngimg.com/uploads/burger_sandwich/burger_sandwich_PNG4135.png',
+      [
+        new Ingredient('Cheese',2),
+        new Ingredient('Buns',2),
+        new Ingredient('Onion',2),
+        new Ingredient('Tomato',1),
+        new Ingredient('Garlic',1)
+      ]
+    ),
+    new Recipe(
+      'Pizza',
+      'Pizza',
+      'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg',
+    [
+      new Ingredient('Cheese',2),
+      new Ingredient('Flour',1),
+      new Ingredient('Tomato',1),
+      new Ingredient('Onion',1),
+      new Ingredient('Mozzaerella',1)
+    ])
   ];
 
   getRecipe(){
